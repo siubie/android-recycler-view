47fdfbff2910a084536642b95a1b3bb080faf839
siubie <putraprima@gmail.com>
2016-09-18 10:30:00 +0700

    layoutmanager

--- RecylcerView/app/src/main/java/com/siubie/recylcerview/MainActivity.java
+++ RecylcerView/app/src/main/java/com/siubie/recylcerview/MainActivity.java
@@ -2,12 +2,20 @@ package com.siubie.recylcerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public RecyclerView mRecyclerView;
+    public RecyclerView.LayoutManager mLayoutManager;
+    public RecyclerView.Adapter mAdapter;
+    public String[] mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
     }
 }
